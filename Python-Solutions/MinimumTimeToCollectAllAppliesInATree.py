f00bfa4181392288faa4e7bd64308bac9c7e4f5f
Bears <97640061+JaredBears@users.noreply.github.com>
2023-01-12 22:04:12 -0600

    January 12, 2023

--- Python-Solutions/MinimumTimeToCollectAllAppliesInATree.py
+++ Python-Solutions/MinimumTimeToCollectAllAppliesInATree.py
@@ -1,16 +1,12 @@
 from typing import List
+from collections import defaultdict
 class Solution:
     # This is a graph problem.  We will create an adjacency list, and then we will traverse the graph.
-    # We will create a function that will check the adjacency list and add the edge to the list.
-    # We will create a function that will traverse the graph and find the apples using DFS.
+    # We will use DFS to traverse the graph.  We will keep track of the total time and the time for each child.
+    # Time Complexity: O(n) where n is the number of nodes to traverse the tree
+    # Space Complexity: O(n) to hold the adjacency list.
     def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
-        adjacency = {}
-        # This function will check the adjacency list and add the edge to the list.
-        def checkAdjacency(edgeOne: int, edgeTwo: int):
-            if edgeOne not in adjacency:
-                adjacency[edgeOne] = set([edgeTwo])
-            else:
-                adjacency[edgeOne].add(edgeTwo)
+        adjacency = defaultdict(set)
         # This function will traverse the graph and find the apples using DFS.
         # For each call, we will pass the node and it's parent node.  
         # We will need to keep track of the total time and the time for each child.
@@ -25,8 +21,7 @@ class Solution:
                 if child_time > 0 or hasApple[child]:
                     total_time += child_time + 2
             return total_time
-
         for x, y in edges: 
-            checkAdjacency(x, y)
-            checkAdjacency(y, x)
+            adjacency[x].add(y)
+            adjacency[y].add(x)
         return findApples(0, -1)
